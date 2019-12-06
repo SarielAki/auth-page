95a55f64d47c5f61e4848b234a8498ef8933b932
SarielAki <sariely@yandex.ru>
2019-12-06 17:53:37 +0300

    Top bar styles

--- src/components/main_page/TopBar.vue
+++ src/components/main_page/TopBar.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="top-bar-wrapper">
         <div class="top-bar">
+            <div class="top-bar-logo">
+                Simple auth
+            </div>
             <nav id="navigation-wrapper">
-                <span class="nav-item__text">Sign in</span>
+                <a class="nav-item nav-item__link sign-in-link">
+                    <span class="nav-item__text">Sign in</span>
+                </a>
             </nav>
         </div>
     </div>
@@ -17,6 +22,11 @@
 <style lang="sass" scoped>
     @import "../../variables"
 
+    %header_text_and_link
+        cursor: pointer
+        color: $header_text_color
+        font-size: $header_font_size
+
     .top-bar-wrapper
         width: 100%
         display: flex
@@ -24,8 +34,39 @@
         justify-content: center
 
     .top-bar
+        display: flex
+        flex-direction: row
+        align-items: center
+        justify-content: space-between
+        width: 100%
+        height: $nav_height
         position: relative
         top: 0
         background: $header_color
+
+        .top-bar-logo
+            @extend %header_text_and_link
+
+    #navigation-wrapper
+
+        .nav-item
+            +deselect
+            position: relative
+            display: flex
+            flex-direction: row
+            align-items: center
+            height: 100%
+            @extend %header_text_and_link
+
+            &, &:hover, &:active, &:focus
+                text-decoration: none
+
+            .nav-item__link
+                transition-duration: 0.2s
+            &:hover
+                color: $main_light_color
+
+            .sign-in-link
+                align-self: end
 
 </style>
