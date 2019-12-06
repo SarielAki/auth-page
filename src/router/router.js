530f2b092ede240c3997846927184e5ebdcbafe8
SarielAki <sariely@yandex.ru>
2019-12-06 18:55:18 +0300

    Auth login component added to router

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import AuthLogin from "@/components/auth/Login";
 
 Vue.use(VueRouter)
 
 const routes = [
+    { path: '/' },
     {
-        path: '/'
+        path: '/login',
+        name: 'loginForm',
+        component: AuthLogin
     }
+
 ]
 
 const router = new VueRouter({
