694a79bf4dca8c1bfb1578d6475103c8d0e2eb32
SarielAki <sariely@yandex.ru>
2019-12-09 12:46:29 +0300

    Imported router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <top-bar></top-bar>
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <top-bar></top-bar>
+        <transition name="fade" mode="out-in" appear>
+            <router-view class="page-content"></router-view>
+        </transition>
+    </div>
 </template>
 
 <script>
 
-import TopBar from "@/components/TopBar";
-export default {
-  name: 'app',
-  components: {TopBar},
-}
+    import TopBar from "@/components/TopBar";
+
+    export default {
+        name: 'app',
+        components: {TopBar},
+    }
 </script>
 
 <style lang="sass">
-  @import "~normalize.css"
-  @import "variables"
+    @import "~normalize.css"
+    @import "variables"
 
-  html, body, #app
-    background: $main_light_color
-    position: relative
-    display: block
-    width: 100%
-    height: 100%
-    top: 0
+    html, body, #app
+        background: $main_light_color
+        position: relative
+        display: block
+        width: 100%
+        height: 100%
+        top: 0
+
+    .page-content
+        z-index: 0
+        display: flex
+        flex-direction: row
+        justify-content: center
 </style>
