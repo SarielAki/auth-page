31ffb19980d49aafc6e237ff3ff981fbb00a044d
SarielAki <sariely@yandex.ru>
2019-12-10 12:54:05 +0300

    Reg component added to router

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import AuthLogin from "@/components/auth/Login";
+import AuthLogin from "@/components/auth/Login"
+import AuthReg from "@/components/auth/Registration"
 
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/' },
+    {path: '/'},
     {
         path: '/login',
         name: 'loginForm',
         component: AuthLogin
+    },
+    {
+        path: '/registration',
+        name: 'regForm',
+        component: AuthReg
     }
 
 ]
